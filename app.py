3b00334cfe4ed79d24650a69f6fea440e8b6d93f
Adityaraj Sahu <adityaraj2019.sahu@gmail.com>
2024-03-01 23:42:01 +0530

    VQA integrated

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 import streamlit as st
 from PIL import Image
 import numpy as np
+from transformers import ViltProcessor, ViltForQuestionAnswering
 
 def main():
     st.markdown("<h1 style='text-align: center; color: white;'>Welcome to True View!</h1>", unsafe_allow_html=True)
+    
+    processor = ViltProcessor.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
+    model = ViltForQuestionAnswering.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
     
     image_file_buffer = st.camera_input("")
     
     if image_file_buffer is not None:
         image = Image.open(image_file_buffer)
-        img_array = np.array(image)
+        # img_array = np.array(image)
         
         if "messages" not in st.session_state:
             st.session_state.messages = []
@@ -21,14 +25,18 @@ def main():
             with st.chat_message(agent):
                 st.write(agent_content)
         
-        user_input = st.chat_input("Please enter your question here...")
+        user_input = st.chat_input("Please enter your question regarding the image here...")
         if user_input:
             with st.chat_message("user"):
                 st.markdown(user_input)
                 
             st.session_state.messages.append({"role": "user", "content": user_input})
             
-            bot_response = "I am a bot, I am here to help you with your questions."
+            encoding = processor(image, user_input, return_tensors="pt")
+            outputs = model(**encoding)
+            logits = outputs.logits
+            idx = logits.argmax(-1).item()
+            bot_response = model.config.id2label[idx]
             with st.chat_message("assistant"):
                 st.markdown(bot_response)
                 
